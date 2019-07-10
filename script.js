6b1593cd3b97444304928657a1420d8be88d033d
Yaika <yaikazeeman@hotmail.com>
2019-07-10 11:53:05 +0200

    obstacles are in an array, not an keypush anymore but on key down, character sprite change by direction

--- script.js
+++ script.js
@@ -30,6 +30,20 @@ class Obstacle {
     }
 }
 
+///////////////////////////////////////////////////////////////////
+// Green monster
+class chaser {
+    constructor(positionX, positionY, width, height, img, callback) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+        this.img = new Image();
+        //this.img.onload = callback;
+        this.img.src = img;
+    }
+}
+
 
 
 ////////////////////////////////////////////////////////////////////
@@ -44,7 +58,6 @@ class Player {
         this.character = new Image();
         this.character.onload = callback;
         this.character.src = character;
-        
     }
 }
 ////////////////////////////////////////////////////////////////////
@@ -52,18 +65,8 @@ class Player {
 window.onload = function() {
     let game = new Game();   
     
-    onkeydown = function(e) {
-        game.savedPosition();
-        switch (e.keyCode) {
-            case 38: game.moveUp();    console.log('up'); break;
-            case 40: game.moveDown();  console.log('down'); break;
-            case 37: game.moveLeft();  console.log('left'); break;
-            case 39: game.moveRight(); console.log('right'); break;
-        }
-        game.checkForCollisionCoin();
-        game.checkForCollisionObstacle();
-        game.updateCanvas();
-    };
+    window.addEventListener("keydown", game.keyDown.bind(game));
+    window.addEventListener("keyup", game.keyUp.bind(game));
 }
 
 class Game {
@@ -71,6 +74,9 @@ class Game {
         this.player = new Player("player1", 0, 0, 30, 30,"sprites-players.png", this.createCoin.bind(this));
         this.canvas = document.getElementById("canvas");
         this.ctx = canvas.getContext("2d");
+        this.currentKey = undefined;
+        this.spritePositionX = 0;
+        this.spritePositionY = 0;
     }
 
     createCoin() {
@@ -78,7 +84,23 @@ class Game {
     }
 
     createObstacle() {
-        this.obstacle = new Obstacle (0, 50, 50, 50, "box.png", this.start.bind(this));
+        let i = 0;
+        this.obstacle = [
+            new Obstacle (0, 50, 50, 50,    "box.png", this.start.bind(this)),
+            new Obstacle (50, 50, 50, 50,   "box.png", this.start.bind(this)),
+            new Obstacle (200, 300, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (200, 350, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (250, 350, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (50, 350, 50, 50,  "box.png", this.start.bind(this)),
+            new Obstacle (50, 300, 50, 50,  "box.png", this.start.bind(this)),
+            new Obstacle (100, 300, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (350, 50, 50, 50,  "box.png", this.start.bind(this)),
+            new Obstacle (350, 100, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (400, 50, 50, 50,  "box.png", this.start.bind(this)),
+            new Obstacle (300, 200, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (250, 200, 50, 50, "box.png", this.start.bind(this)),
+            new Obstacle (50, 200, 50, 50,  "box.png", this.start.bind(this))
+        ]
     }
 
     savedPosition(){
@@ -87,18 +109,35 @@ class Game {
     }
 
     start() {
-        this.intervalId = setInterval(this.updateCanvas.bind(this), 100)
+        this.intervalId = setInterval(this.updateCanvas.bind(this), 20)
     }
   
     draw() {
         this.ctx.drawImage(this.coin.img, this.coin.positionX, this.coin.positionY, 30, 30);
-        this.ctx.drawImage(this.player.character, 0, 0, 30, 30, this.player.positionX, this.player.positionY, 30, 30);
+        this.ctx.drawImage(this.player.character, this.spritePositionX, this.spritePositionY, 32, 32, this.player.positionX, this.player.positionY, 32, 32);
 
-        this.ctx.drawImage(this.obstacle.img, this.obstacle.positionX, this.obstacle.positionY, 50, 50);
+        let i = 0;
+        for(i = 0; i < this.obstacle.length; i++){
+            this.ctx.drawImage(this.obstacle[i].img, this.obstacle[i].positionX, this.obstacle[i].positionY, 50, 50);
+        }
+        
     }
+
+    collisionDetection() {
+        this.checkForCollisionCoin();
+        this.checkForCollisionObstacle();
+    };
 
     updateCanvas() {
         this.clearCanvas();
+        this.savedPosition();
+        switch (this.currentKey) {
+            case 38: this.moveUp();    console.log('up'); break;
+            case 40: this.moveDown();  console.log('down'); break;
+            case 37: this.moveLeft();  console.log('left'); break;
+            case 39: this.moveRight(); console.log('right'); break;
+        }
+        this.collisionDetection();
         this.draw();
     }
 
@@ -119,48 +158,81 @@ class Game {
     }
 
     checkForCollisionObstacle() {
-        if (this.player.positionX < this.obstacle.positionX + this.obstacle.width &&
-            this.player.positionX + this.player.width > this.obstacle.positionX &&
-            this.player.positionY < this.obstacle.positionY + this.obstacle.height && 
-            this.player.positionY + this.player.height > this.obstacle.positionY){
-                this.player.positionX = this.currentX;
-                this.player.positionY = this.currentY;
-                return true;
-            }
+        for(let i = 0; i < this.obstacle.length; i++){
+            if (this.player.positionX < this.obstacle[i].positionX + this.obstacle[i].width &&
+                this.player.positionX + this.player.width > this.obstacle[i].positionX &&
+                this.player.positionY < this.obstacle[i].positionY + this.obstacle[i].height && 
+                this.player.positionY + this.player.height > this.obstacle[i].positionY){
+                    this.player.positionX = this.currentX;
+                    this.player.positionY = this.currentY;
+                    return true;
+                }
+        }
     }
 
     moveCoin(){
         this.coin.positionX = Math.floor(Math.random() * (this.canvas.width - this.coin.width)); 
         this.coin.positionY = Math.floor(Math.random() * (this.canvas.height - this.coin.width)); 
+        for(let i = 0; i < this.obstacle.length; i++){
+            if(this.coin.positionX < this.obstacle[i].positionX + this.obstacle[i].width &&
+                this.coin.positionX + this.coin.width > this.obstacle[i].positionX &&
+                this.coin.positionY < this.obstacle[i].positionY + this.obstacle[i].height && 
+                this.coin.positionY + this.coin.height > this.obstacle[i].positionY){
+                    this.moveCoin();
+                }
+        }
     }
 
     moveUp() {      
-        if(this.player.positionY  - this.player.height  < 0){
+        if(this.player.positionY < 0){
         this.player.positionY = 0;
         }else{
-         this.player.positionY -= 20 }
+         this.player.positionY -= 1/3;
+         this.spritePositionX = 0;
+         this.spritePositionY = 96;
+         }
         };
 
     moveDown() { 
         if(this.player.positionY  + this.player.height >= this.canvas.height) {
             this.player.positionY = this.canvas.height - this.player.height;
         }else{
-        this.player.positionY += 20} 
+        this.player.positionY += 1/3;
+        this.spritePositionX = 0;
+        this.spritePositionY = 0;
+        } 
         };
 
     moveLeft() {
-        if(this.player.positionX - this.player.width < 0){
+        if(this.player.positionX< 0){
             this.player.positionX = 0;
         }else{
-         this.player.positionX -= 20 }
+         this.player.positionX -= 1/3;
+         this.spritePositionX = 0;
+         this.spritePositionY = 32;
+        }
         };
 
     moveRight() { 
         if(this.player.positionX + this.player.width >= this.canvas.width) {
             this.player.positionX = this.canvas.width - this.player.width;
         }else{
-        this.player.positionX += 20 }
+        this.player.positionX += 1/3;
+        this.spritePositionX = 0;
+        this.spritePositionY = 64;
+        }
         };
+
+    keyDown(e) {
+        this.currentKey = e.keyCode;
+    };
+        
+    keyUp(e) {
+        // the if check here allows pressing a second key before releasing the old one
+        if (this.currentKey === e.keyCode) {
+        this.currentKey = undefined;
+        }
+    };
 
 }
 
